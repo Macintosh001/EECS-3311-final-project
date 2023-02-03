5fd6d5802fac51fcd8d7a10442d876ac4c8647d5
Lucas Woolner <lwoolner8@gmail.com>
2023-02-03 15:16:04 -0800

    Database stub and draft of interface

--- app/src/main/java/project/persistence/Database.java
+++ app/src/main/java/project/persistence/Database.java
@@ -1,115 +1,51 @@
 package project.persistence;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import project.objects.Product;
+import project.objects.ProductList;
 
 /**
- * This Class implements database connections and some basic DB functionality needed for an inventory system.
- * It can create the database and relational schema for the inventory system for new users.
- * Note that this class must be instantiated for use, as this was seen as easier than passing
- * Connection objects around static methods. That said, it can be easily modified to use static methods
- * if the need arises.
+ * The database interface contains operations for interacting with a database.
+ * It is meant to be implemented by a class that handles database operations and a
+ * database stub for testing purposes. This way, business logic can communicate with the interface
+ * while not knowing if it is communicating with a stub or true database.
  */
-public class Database {
-    private Connection con; //the connection to be made and terminated
-    private String url; //the url of the server or database to be connected to
-    private String username; //the username of the user connecting
-    private String password; //the password of the connection
+public interface Database {
 
-    public Database(){
-        //initialize some connection information
-        url = "jdbc:mysql://localhost:3306/Assignment_EECS_3311";
-        username = "root";
-        password = "root1234";
-    }
+
+    //Query DB or stub
 
     /**
-     * Connect to a server, but not any specific database on that server.
-     * Used to create an empty inventory database for new users.
-     * Note: may be updated to return flags of success/failure
-     * @throws SQLException
+     * Gets a product from the db/stub with a given barcode
+     * @param barcode: the barcode of a product
+     * @return a product with the barcode.
      */
-    public void connectToServer(){
-        url = "jdbc:mysql://localhost:3306/";
-        try {
-            con = DriverManager.getConnection(url, username, password);
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-    }
+    public Product getProduct(Integer barcode);
 
     /**
-     * connect to the database for the inventory system on a local server.
-     * @throws: SQLException
-     */
-    public void connect(){
-        url = "jdbc:mysql://localhost:3306/Assignment_EECS_3311";
-        try {
-            con = DriverManager.getConnection(url, username, password);
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * End the database connection associated with the instantiation of this class.
-     * Should be used every time a database operation is complete to avoid privacy leaks.
-     * @pre: a connection is in place
-     * @throws: SQLException
      *
+     * @return a productList representing the objects of a db/stub
      */
-    public void terminate(){
-        try {
-            con.close(); //close database connection
-        } catch(SQLException c) {
-            c.printStackTrace();
-        }
-    }
+    public ProductList getProductList();
+
+
+    //Modification of DB or stub
 
     /**
-     * Creates an inventory database on a local server for first-time users of the inventory system.
-     * Should be used only once: that is, when the user first uses the inventory system.
-     * @pre: the user does not already have a database of the specified name or a schema build on that database.
-     * @throws: SQLException
+     * Add a product to db or stub
+     * @param product : the product added
      */
-    public void createDB(){
-        try {
-            this.connectToServer();
-            Statement statement = con.createStatement();
-            statement.execute("create schema Assignment_EECS_3311");
-            this.terminate();
-            this.buildDBSchema();
-
-        } catch(SQLException c) {
-            c.printStackTrace();
-        }
-    }
+    public void addProduct(Product product);
 
     /**
-     * Initializes the inventory database. That is, creates a working, but empty, inventory system
-     * on the database with create table statements. Must be called, or the equivalent actions taken on the mySQL server
-     * directly, before anything is added/removed/queried to/from the database.
-     * @throws: SQLException
-     *
+     * Search for and remove a product from the db or stub
+     * @param barcode: the barcode of the removed product
      */
-    private void buildDBSchema(){
-        try {
-            this.connect();
-            Statement statement = con.createStatement();
-            statement.execute("create table PRODUCT" +
-                    "(BARCODE INT primary key not null," +
-                    "NAME VARCHAR(500)," +
-                    "PRICE REAL not null check(price >= 0) default(0), " +
-                    "QUANTITY INT not null default(0), " +
-                    "EXPIRY_DATE DATE)");
-            this.terminate();
-        } catch(SQLException c) {
-            c.printStackTrace();
-        }
-    }
+    public void removeProduct(Integer barcode);
+
+    /**
+     * Replace a product in the db/stub with an updated version of itself.
+     * @param product: a product
+     */
+    public void replaceProduct(Product product);
 
 }
