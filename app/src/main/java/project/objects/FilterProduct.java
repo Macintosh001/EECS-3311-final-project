fb3af0f8f6eb99fc187f36873940ad078fe11307
Lucas Woolner <lwoolner8@gmail.com>
2023-02-16 10:29:14 -0800

    refactor filter object

--- app/src/main/java/project/objects/FilterProduct.java
+++ app/src/main/java/project/objects/FilterProduct.java
@@ -10,15 +10,51 @@ package project.objects;
 public class FilterProduct {
 
     //Values barcode, name, quantity, etc.
-    private final String filterTpe;
+    private final String filterType;
 
     //objects of type Integer, Float, Date or String only. Objects should be of the same type.
     private final Object rangeStart;
     private final Object rangeEnd;
 
-    public FilterProduct(String filterType, Object rangeStart, Object rangeEnd){
-        this.filterTpe = filterType;
-        //add some object type checking for stability
+    /**
+     * Static factory method that builds and returns a ProductFilter if params are acceptable
+     * rangeStart and end must have the same class type;
+     * @param filterType type of filter
+     * @param rangeStart: start of range included in filter
+     * @param rangeEnd: end of range included in filter
+     * @return a productFilter ot null if params are not valid to create a filter
+     */
+    public static FilterProduct FilterProductFactory(String filterType, Object rangeStart, Object rangeEnd){
+        if(rangeStart.getClass().getName().compareTo(rangeEnd.getClass().getName()) != 0){
+           return null;
+        }
+        else if(rangeStart.getClass().getName().compareTo("String") != 0
+                && rangeStart.getClass().getName().compareTo("Integer") != 0
+                && rangeStart.getClass().getName().compareTo("Float") != 0
+                && rangeStart.getClass().getName().compareTo("Date") != 0){
+            return null;
+        }
+        else if(rangeEnd.getClass().getName().compareTo("String") != 0
+                && rangeEnd.getClass().getName().compareTo("Integer") != 0
+                && rangeEnd.getClass().getName().compareTo("Float") != 0
+                && rangeEnd.getClass().getName().compareTo("Date") != 0){
+            return null;
+        }
+        else if(filterType.toLowerCase().compareTo("quantity") != 0
+                && filterType.toLowerCase().compareTo("name") != 0
+                && filterType.toLowerCase().compareTo("barcode") != 0
+                && filterType.toLowerCase().compareTo("expiryDate") != 0
+                && filterType.toLowerCase().compareTo("price") != 0){
+            return null;
+        }
+        else{
+            return new FilterProduct(filterType, rangeStart, rangeEnd);
+        }
+
+    }
+
+    private FilterProduct(String filterType, Object rangeStart, Object rangeEnd){
+        this.filterType = filterType;
         this.rangeStart = rangeStart;
         this.rangeEnd = rangeEnd;
     }
@@ -27,8 +63,8 @@ public class FilterProduct {
         return rangeEnd;
     }
 
-    public String getFilterTpe() {
-        return filterTpe;
+    public String getFilterType() {
+        return filterType;
     }
 
     public Object getRangeStart() {
