801ec2a0fffd1ccfd901f496f4e3020a5adb1ad5
kofi12 <kofi12@my.yorku/ca>
2023-02-16 10:59:28 -0500

    fixed overlapping field issue in UpdateDialog

--- app/src/main/java/project/display/UpdateDialog.java
+++ app/src/main/java/project/display/UpdateDialog.java
@@ -32,32 +32,31 @@ public class UpdateDialog extends JDialog {
         add(barcodeInput);
 
         JLabel nameInputLabel = new JLabel("Name:");
-
-        nameInputLabel.setBounds(10, 10, 200, 50);
+        nameInputLabel.setBounds(10, 70, 200, 50);
         add(nameInputLabel);
 
         JTextField nameInput = new JTextField();
-        nameInput.setBounds(220, 10, 200, 50);
+        nameInput.setBounds(220, 70, 200, 50);
         add(nameInput);
 
         JLabel quantityInputLabel = new JLabel("Quantity:");
-        quantityInputLabel.setBounds(10, 70, 200, 50);
+        quantityInputLabel.setBounds(10, 130, 200, 50);
         add(quantityInputLabel);
 
         JTextField quantityInput = new JTextField();
-        quantityInput.setBounds(220, 70, 200, 50);
+        quantityInput.setBounds(220, 130, 200, 50);
         add(quantityInput);
 
         JLabel priceInputLabel = new JLabel("Price: ");
-        priceInputLabel.setBounds(10, 130, 200, 50);
+        priceInputLabel.setBounds(10, 190, 200, 50);
         add(priceInputLabel);
 
         JTextField priceInput = new JTextField();
-        priceInput.setBounds(220, 130, 200, 50);
+        priceInput.setBounds(220, 190, 200, 50);
         add(priceInput);
 
         JLabel dateInputLabel = new JLabel("Expiry Date:");
-        dateInputLabel.setBounds(10, 200, 200, 50);
+        dateInputLabel.setBounds(10, 250, 200, 50);
         add(dateInputLabel);
 
         display.getTable().addMouseListener(new MouseAdapter() {
@@ -79,7 +78,7 @@ public class UpdateDialog extends JDialog {
         JButton datePickerButton = datePicker.getComponentToggleCalendarButton();
         datePickerButton.setText("");
         datePickerButton.setIcon(dateExampleIcon);
-        datePicker.setBounds(220,200,200,50);
+        datePicker.setBounds(220,250,200,50);
         add(datePicker);
 
 
