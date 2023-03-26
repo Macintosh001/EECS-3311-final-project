e8d9e74ce24c994fa36dc68fbd86227bf2b84ea1
Lucas Woolner <lwoolner8@gmail.com>
2023-03-26 14:09:18 -0700

    RestockTask database implemented

--- app/src/main/java/project/persistence/RestockTaskDatabaseStub.java
+++ app/src/main/java/project/persistence/RestockTaskDatabaseStub.java
@@ -13,13 +13,22 @@ public class RestockTaskDatabaseStub implements RestockTaskDatabase{
        this.dbStub = this.getTestData();
    }
 
+    /**
+     * Get fixed starting data for stub
+     * @return arrayList of starting data
+     */
    private ArrayList<RestockTask> getTestData(){
        ArrayList<RestockTask> restockTasks = new ArrayList<>();
-       restockTasks.add(new RestockTask("oreo", 5, 20));
+       restockTasks.add(new RestockTask("oreos", 5, 20));
        restockTasks.add(new RestockTask("cheetos", 3, 15));
        return restockTasks;
    }
 
+    /**
+     * get a task from database
+     * @param name name of task retrieved
+     * @return task with the given name, or null if no such task exists
+     */
     @Override
     public RestockTask getRestockTask(String name) {
         for(RestockTask r : dbStub){
@@ -30,11 +39,18 @@ public class RestockTaskDatabaseStub implements RestockTaskDatabase{
         return null;
     }
 
+    /**
+     * return a complete set of tasks currently in the database
+     */
     @Override
     public List<RestockTask> getRestockTaskList() {
         return dbStub;
     }
 
+    /**
+     * remove a task from database
+     * @param name of task to be removed
+     */
     @Override
     public void removeRestockTask(String name) {
         RestockTask restock = null;
@@ -49,12 +65,28 @@ public class RestockTaskDatabaseStub implements RestockTaskDatabase{
         }
     }
 
+    /**
+     * Update RestockTask
+     * @param res updated task
+     */
     @Override
     public void replaceRestockTask(RestockTask res) {
         this.removeRestockTask(res.getName());
         this.addRestockTask(res);
     }
 
+    /**
+     * Add task to db
+     * @param mod a task to be added
+     */
     @Override
     public void addRestockTask(RestockTask res) {dbStub.add(res);}
+
+    /**
+     * make the database empty
+     */
+    @Override
+    public void empty(){
+       this.dbStub = new ArrayList<>();
+    }
 }
